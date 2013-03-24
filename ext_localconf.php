28044cde58aa82d2ac695ee19d44881c9926c168
Felix Nagel <info@felixnagel.com>
2013-03-24 14:50:07 +0100

    [BUGFIX] adjust scheduler task registration namespacing

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,13 +5,13 @@ if (!defined('TYPO3_MODE')) {
 }
 
 if (TYPO3_MODE == 'BE') {
-	$TYPO3_CONF_VARS['SC_OPTIONS']['scheduler']['tasks']['tx_damfalmigration_task_migratetask'] = array(
+	$TYPO3_CONF_VARS['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\DamFalmigration\\Task\\MigrateTask'] = array(
 		'extension'        => $_EXTKEY,
 		'title'            => 'DAM-FAL Migration: Migrate DAM Records to FAL Records',
 		'description'      => 'Migrates all available DAM records from fileadmin/ to the local storage of the FAL records.',
 	);
 
-	$TYPO3_CONF_VARS['SC_OPTIONS']['scheduler']['tasks']['tx_damfalmigration_task_migraterelationstask'] = array(
+	$TYPO3_CONF_VARS['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\DamFalmigration\\Task\\MigrateRelationsTask'] = array(
 		'extension'        => $_EXTKEY,
 		'title'            => 'DAM-FAL Migration: Migrate DAM Relations to FAL Relations',
 		'description'      => 'Migrates all available DAM relations from tt_content textpic/image/uploads to FAL relations.',
