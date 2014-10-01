cbb8e8155ec6cfee5a210fe000e0a82c90d90b17
Michiel Roos <michiel@maxserv.nl>
2014-10-01 15:24:33 +0200

    [TASK] Cleanup [TASK] Add $parent property to AbstractService

--- Classes/Controller/DamMigrationCommandController.php
+++ Classes/Controller/DamMigrationCommandController.php
@@ -2,32 +2,33 @@
 namespace B13\DamFalmigration\Controller;
 
 /**
- *  Copyright notice
+ * Copyright notice
  *
- *  (c) 2013 Benjamin Mack <typo3@b13.de>
- *  All rights reserved
+ * (c) 2013 Benjamin Mack <typo3@b13.de>
+ * All rights reserved
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is free
- *  software; you can redistribute it and/or modify it under the terms of
+ * This script is part of the TYPO3 project. The TYPO3 project is free
+ * software; you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any
  * later version.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
  *
- *  This script is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This script is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
  *
- *  This copyright notice MUST APPEAR in all copies of the script!
+ * This copyright notice MUST APPEAR in all copies of the script!
  */
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 use TYPO3\CMS\DamFalmigration\Service;
+use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
 
 // I can haz color / use unicode?
 if (DIRECTORY_SEPARATOR !== '\\') {
@@ -51,7 +52,7 @@ if (@exec('tput cols')) {
  * @package B13\DamFalmigration
  * @subpackage Controller
  */
-class DamMigrationCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\CommandController {
+class DamMigrationCommandController extends CommandController {
 
 	/**
 	 * Migrates all DAM records to FAL. A DB field "_migrateddamuid" connects each FAL record to the original DAM record.
