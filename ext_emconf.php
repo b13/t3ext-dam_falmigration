2ce5cea7520ddf8e0ecf3155217dd212485f1d0e
Benjamin Mack <benni@typo3.org>
2015-01-05 14:11:50 +0100

    [TASK] Updated version in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,14 +14,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Migrate DAM Records and references to FAL references',
 	'description' => 'Takes the DAM records and references (for tt_content right now) and migrates them into the local fileadmin/ FAL storage.',
 	'category' => 'misc',
-	'author' => 'Benjamin Mack',
+	'author' => 'Benjamin Mack, Michiel Roos and a lot others',
 	'author_email' => 'benni@typo3.org',
 	'shy' => '',
 	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.4',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.2.0-6.2.99',
@@ -39,5 +39,4 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"64e1";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"b6d7";s:20:"doc/wizard_form.html";s:4:"7bb0";}',
 );
